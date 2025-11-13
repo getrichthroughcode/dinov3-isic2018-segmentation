f88bdf7604e55eb48a59c39e174e15f06c5e46c1
Abdoulaye Diallo <abdoulayediallo338@gmail.com>
2025-11-13 15:11:23 +0100

    fix matrice multiplication type bug

--- src/dinoseg/models/dino_v3_unet.py
+++ src/dinoseg/models/dino_v3_unet.py
@@ -38,7 +38,7 @@ class ConvBlock(nn.Module):
             nn.Conv2d(in_ch, out_ch, 3, padding=1),
             nn.BatchNorm2d(out_ch),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            nn.Conv2d(out_ch, out_ch, 3, padding=1),
             nn.BatchNorm2d(out_ch),
             nn.ReLU(inplace=True),
         )
